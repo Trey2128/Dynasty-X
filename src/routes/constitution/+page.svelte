03f827164d31a51a1f6dadcce25138e83f97628b
Trey2128 <ratpackaustin@yahoo.com>
2026-09-12 13:57:03 -0500

    Refactor sections and update content structure

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,12 +1,12 @@
 <script>
     import { dues } from '$lib/utils/helper';
-    let one, oneOne, oneOneOne, oneTwo, oneTwoOne, oneTwoTwo, oneTwoThree, oneTwoFour, oneTwoFive, oneThree;
-    let two, twoOne, twoTwo, twoThree;
-    let three, threeOne;
+    let one, oneOne, oneTwo, oneTwoOne, oneThree, oneFour, oneFourOne, oneFourTwo, oneFourThree, oneFourFour, oneFive, oneFiveOne, oneFiveTwo, oneFiveThree, oneSix, oneSeven;
+    let two, twoOne, twoTwo, twoTwoOne, twoThree, twoFour, twoFive;
+    let three, threeOne, threeTwo, threeThree;
     let four, fourOne, fourTwo;
-    let five, fiveOne;
-    let six, sixOne, sixTwo, sixThree;
-    let seven, sevenOne, sevenTwo, sevenThree;
+    let five, fiveOne, fiveTwo, fiveThree, fiveFour;
+    let six, sixOne, sixOneOne, sixOneTwo, sixOneThree;
+    let seven, sevenOne, sevenTwo, sevenThree; 
 
     const goToSection = (section) => {
         const top = section.getBoundingClientRect().top + window.pageYOffset;
@@ -102,50 +102,71 @@
     
     <h3 class="noUnderscore clickable" onclick={() => goToSection(one)}>Section 1: Roster</h3>
         <h4 class="noUnderscore clickable" onclick={() => goToSection(oneOne)}>1.1 Positional Breakdown</h4>
-            <h5 class="noUnderscore clickable" onclick={() => goToSection(oneOneOne)}>1.1.1 Position Maximums</h5>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneTwo)}>1.2 Trading</h4>
-            <h5 class="noUnderscore clickable" onclick={() => goToSection(oneTwoOne)}>1.2.1 Trade Collusion</h5>
-            <h5 class="noUnderscore clickable" onclick={() => goToSection(oneTwoTwo)}>1.2.2 Lending Players Prohibition</h5>
-            <h5 class="noUnderscore clickable" onclick={() => goToSection(oneTwoThree)}>1.2.3 Trade Deadline</h5>
-            <h5 class="noUnderscore clickable" onclick={() => goToSection(oneTwoFour)}>1.2.4 Trade Restrictions</h5>
-            <h5 class="noUnderscore clickable" onclick={() => goToSection(oneTwoFive)}>1.2.5 Veto/Trade Process</h5>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneThree)}>1.3 Waiver Wire</h4>
-    
-    <h3 class="noUnderscore clickable" onclick={() => goToSection(two)}>Section 2: Drafting</h3>
-    
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoOne)}>2.1 Draft Order</h4>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoTwo)}>2.2 Rookie Drafts</h4>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoThree)}>2.3 Draft Day Trades</h4>
-    
-    <h3 class="noUnderscore clickable" onclick={() => goToSection(three)}>Section 3: Scoring System</h3>
-    
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(threeOne)}>3.1 Voting on Scoring System</h4>
-    
-    <h3 class="noUnderscore clickable" onclick={() => goToSection(four)}>Section 4: Postseason</h3>
-    
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(fourOne)}>4.1 Playoffs</h4>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(fourTwo)}>4.2 Playoff Tiebreakers</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneTwo)}>1.2 Taxi Squad/IR</h4>
+            <h5 class="noUnderscore clickable" onclick={() => goToSection(oneTwoOne)}>1.2.1 Taxi Squad Stealing</h5>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneThree)}>1.3 Dynasty</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFour)}>1.4 Trading</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFourOne)}>1.4.1 Trade Collusion</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFourTwo)}>1.4.2 Trade Deadline</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFourThree)}>1.4.3 Trade Restrictions</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFourFour)}>1.4.4 Trade Processing</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFive)}>1.5 Waiver Wire</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFiveOne)}>1.5.1 Free Agent Auction Budget (FAAB)</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFiveTwo)}>1.5.2 Offseason Waivers</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(oneFiveThree)}>1.5.3 Season Waivers</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneSix)}>1.6 Divisional Breakdown</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(oneSix)}>1.7 Schedule</h4>
 
-    <h3 class="noUnderscore clickable" onclick={() => goToSection(five)}>Section 5: Tanking Policy</h3>
-    
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(fiveOne)}>5.1 League Tanking Policy</h4>
 
-    <h3 class="noUnderscore clickable" onclick={() => goToSection(six)}>Section 6: Replacing Managers</h3>
+
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(two)}>Section 2: Owner Expectations</h3>
     
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(sixOne)}>6.1 Removing Managers</h4>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(sixTwo)}>6.2 Replacing Managers</h4>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(sixThree)}>6.3 Replacement Incentive</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoOne)}>2.1 Setting Lineups</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoTwo)}>2.2 Leaving the League</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(twoTwoOne)}>2.2.1 New Owners</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoThree)}>2.3 Removal</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoFour)}>2.4 Rule Voting</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(twoFive)}>2.5 Last Place Punishment</h4>
+
+
+    
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(three)}>Section 3: Drafting</h3>
+    
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(threeOne)}>3.1 Draft Order</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(threeTwo)}>3.2 Draft Day Trades</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(threeThree)}>3.3 Draft Date & Time</h4>
+
+    
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(four)}>Section 4: Scoring System</h3>
+    
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(fourOne)}>4.1 Scoring</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(fourTwo)}>4.2 Voting on Scoring System</h4>
+
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(five)}>Section 5: Postseason</h3>
+    
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(fiveOne)}>5.1 Playoffs</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(fiveTwo)}>5.2 Playoff Tiebreakers</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(fiveThree)}>5.3 Seeding Tiebreakers</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(fiveFour)}>5.4 Toilet Bowl</h4>
+
+
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(six)}>Section 6: Expansion</h3>
+    
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(sixOne)}>6.1 Expansion Rules</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(sixOneOne)}>6.1.1 Setup</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(sixOneTwo)}>6.1.2 The Expansion Draft</h4>
+            <h4 class="noUnderscore clickable" onclick={() => goToSection(sixOneThree)}>6.1.3 After the Draft</h4>
 
     <h3 class="noUnderscore clickable" onclick={() => goToSection(seven)}>Section 7: League Finances</h3>
     
         <h4 class="noUnderscore clickable" onclick={() => goToSection(sevenOne)}>7.1 League Dues</h4>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(sixTwo)}>7.2 Payout</h4>
-        <h4 class="noUnderscore clickable" onclick={() => goToSection(sixThree)}>7.3 Raising Dues</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(sevenTwo)}>7.2 Payouts</h4>
+        <h4 class="noUnderscore clickable" onclick={() => goToSection(sevenThree)}>7.3 Raising Dues</h4>
     
     
     <hr />
     
-    <h2 class="sectionHeading" bind:this={one}>Section 1 Roster Breakdown</h2>
+    <h2 class="sectionHeading" bind:this={one}>Section 1 Roster</h2>
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Positional Breakdown</h3>
     
@@ -162,33 +183,21 @@
         <li>TE</li>
         <li>FLEX (RB/WR/TE)</li>
         <li>FLEX (RB/WR/TE)</li>
+        <li>SUPERFLEX (QB/RB/WR/TE)</li>
     </ul>
     
     <p>12 Bench</p>
 
+    <p>4 Taxi Squad Spots (Rookies or 2nd year players only)</p>
+
     <p>2 IR Spots (Player must be labeled as IR, Out, DNR, or Covid in the Sleeper App)</p>
 
-    <p>4 Taxi Squad Spots (Rookies or 2nd year players only)</p>
-    
-        <h4 bind:this={oneOneOne}>1.1.1 Position Maximums</h4>
-        
-        <div class="subBlock">
-            <table class="positionMaximums">
-                <tbody>
-                <tr><td>QB</td><td class="right">3 active, 5 total</td></tr>
-                <tr><td>RB</td><td class="right">20</td></tr>
-                <tr><td>WR</td><td class="right">20</td></tr>
-                <tr><td>TE</td><td class="right">10</td></tr>
-                <tr><td>D/ST</td><td class="right">3</td></tr>
-                <tr><td>K</td><td class="right">3</td></tr>
-                </tbody>
-            </table>
-        </div>
 
-
-    <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Trading</h3>
+    <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Taxi Squad/IR</h3>
     
-    <p>Trading of players and draft picks (up to 3 drafts away) are allowed. Trades will process immediately and will be reversed if vetoed or under investigation for collusion. Tradable assets include players, draft picks, and FAAB $.</p>
+    <p>The Taxi Squad are development roster spots where you can put 4 rookies or 2nd year players. Any player in their 3rd year or later is ineligible for the Taxi Squad. Taxi Squads lock at the start of the first game of the season. At any point during the season you can call a Taxi Squad player up to your active roster. The Taxi Squad roster spot cannot be refilled until the next offseason. </p>
+   
+    <p>The 2 Injury Reserve spots can be filled by anyone who’s out, suspended, holding out, or on Covid-19 reserve. Leaving healthy players on IR will result in punishments referred to in section 2.2.</p>
     
     <h4 bind:this={oneTwoOne}>1.2.1 Trade Collusion</h4>
     <div class="subBlock">
